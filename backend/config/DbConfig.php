d796259eadbf681de85e87f3d133152822f3701a
JustInTimez <just.oost@gmail.com>
2023-02-16 19:28:39 +0200

    Setting up db connection

---
+++ backend/config/DbConfig.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Config;
+
+class DatabaseConfig {
+    
+
+    private $host = "localhost";
+    private $username = "root";
+    private $password = "root";
+    private $dbName = "pokemon_app";
+
+
+
+    public function connect() {
+
+        $conn = new \mysqli($this->host, $this->username, $this->password, $this->dbName);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error); // Die function to close connection in case of error
+        } else {
+            return $conn;
+        }
+    }
+
+}

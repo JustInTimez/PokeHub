9dabfc48fee6d165266ccf396f1bebb782ba64f0
JustInTimez <just.oost@gmail.com>
2023-03-01 23:30:12 +0200

    Adds typewriter effect

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -6,6 +6,50 @@ import { pages } from "./components/pagination.js";
 let favorites = [];
 let currentPage = 1; // initialize currentPage variable for pagination
 
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+const textArray = ["Welcome, Trainers!", "Wanna be the very best!"];
+const typingDelay = 100;
+const erasingDelay = 60;
+const newTextDelay = 700;      // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+      if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+      typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+      charIndex++;
+      setTimeout(type, typingDelay);
+  }
+  else {
+      cursorSpan.classList.remove("typing");
+      setTimeout(erase, newTextDelay);
+  }
+};
+
+function erase() {
+  if (charIndex > 0) {
+      if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+      typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+      charIndex--;
+      setTimeout(erase, erasingDelay);
+  }
+  else {
+      cursorSpan.classList.remove("typing");
+      textArrayIndex++;
+      if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+      setTimeout(type, typingDelay + 1100);
+  }
+};
+
+// On DOM Load, initiate the effect of typing at the top of the page
+document.addEventListener("DOMContentLoaded", function () {    
+  if (textArray.length) setTimeout(type, newTextDelay + 100);
+});
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
   const limit = 20; // set the limit per page (pagination)
   let offset = 0; // set the initial offset (pagination)
